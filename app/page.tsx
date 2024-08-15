71016639b5c2f38e71100422836d1189c3069f25
Muhammad Hasan <79955819+sanryuu03@users.noreply.github.com>
2024-08-15 19:57:07 +0700

    success count
    posisi di tengah layar secara vertikal dan horizontal.
    Flexbox Layout: Menggunakan flex, items-center, dan justify-center di div utama untuk memastikan semua konten berada di tengah layar baik secara vertikal maupun horizontal

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 import NavbarMobile from "@/components/navbar/mobile/NavbarMobile";
 import { useState } from "react";
+import { FcPlus, FcMinus } from "react-icons/fc";
+import { VscDebugRestart } from "react-icons/vsc";
 
 export default function Home() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState<number>(0);
 
     return (
-        <div className="min-h-dvh mx-auto max-w-screen-sm border-2 border-lime-500 rounded-xl bg-sky-300">
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
-            <NavbarMobile/>
+        <div className="min-h-dvh mx-auto max-w-screen-sm border-2 border-lime-500 rounded-xl bg-sky-300 flex flex-col items-center justify-center">
+            <div className="text-center mb-4">
+                <p className="text-xl font-semibold">You clicked {count} times</p>
+            </div>
+            <div className="flex space-x-32">
+                <button onClick={() => setCount((currentNumber) => currentNumber - 1)} className="focus:outline-none">
+                    <FcMinus size={100} />
+                </button>
+                <button onClick={() => setCount((currentNumber) => currentNumber + 1)} className="focus:outline-none">
+                    <FcPlus size={100} />
+                </button>
+            </div>
+            <div>
+                <button onClick={() => setCount(0)} className="focus:outline-none">
+                    <VscDebugRestart size={100} />
+                </button>
+            </div>
+            <NavbarMobile />
         </div>
     )
 }
