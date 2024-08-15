2fd2f31b913f578f0cf42ee532c36206c7b15a0a
Muhammad Hasan <79955819+sanryuu03@users.noreply.github.com>
2024-08-15 19:15:21 +0700

    tampilan mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
+import NavbarMobile from "@/components/navbar/mobile/NavbarMobile";
 import { useState } from "react";
 
 export default function Home() {
     const [count, setCount] = useState(0);
 
     return (
-        <div className="min-h-dvh">
+        <div className="min-h-dvh mx-auto max-w-screen-sm border-2 border-lime-500 rounded-xl bg-sky-300">
             <p>You clicked {count} times</p>
             <button onClick={() => setCount(count + 1)}>
                 Click me
             </button>
+            <NavbarMobile/>
         </div>
     )
 }
